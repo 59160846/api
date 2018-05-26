07b291efe4e14f4b99914589aa59f1a5dc7b6e73
Your Name <59160846@go.buu.ac.th>
2018-05-26 15:57:15 +0700

    kanapoj

--- server.js
+++ server.js
@@ -8,17 +8,17 @@ let contacts = [
     {name:"tantai",phoneNumber:"096121231"},
     {name:"kanapoj",phoneNumber:"0636251929"}
 ]
+///คณพศ 59160846
 
-
-/// TODO: Develop GET /contacts
+/// TODO: Develop GET /contacts 
 app.use(bodyParser.json())
 app.use(cors())
-
+///เเทนไท 59160764
 
 app.get('/contacts',(req,res) => {
     res.json(contacts)
 })
-
+///เเทนไท 59160764
 
 /// TODO: Develop POST /contacts
 app.post('/contacts',(req,res)=>{
@@ -26,7 +26,7 @@ app.post('/contacts',(req,res)=>{
     contacts.push(newContact)
     res.status(201).json(newContact)
 })
-
+///คณพศ 59160846
 
 app.listen(3000, () => {
     console.log('API Server started at port 3000')
